ce91053cc02dd43f2efb0a582a07c80551bb9f7c
Javier Caselli <7252806+javiercaselli@users.noreply.github.com>
2025-11-18 18:02:04 +0100

    Ejercicios tema 8 (funciones) - Subida 2

--- Tema8Funciones/T08E06Conversiones.java
+++ Tema8Funciones/T08E06Conversiones.java
@@ -10,9 +10,41 @@ public class T08E06Conversiones {
      * @param entrada Entrada binaria
      * @return Salida decimal
      */
-    public static int binarioADecimal(String entrada) {
+    public static String binarioADecimal(String entrada) {
         int salida = Integer.parseInt(entrada, 2);
-        return salida;
+        return Integer.toString(salida);
+    }
+
+    /**
+     * Función que convierte de binario a octal.
+     * 
+     * @param entrada Entrada binaria
+     * @return Salida octal
+     */
+    public static String binarioAOctal(String entrada) {
+        int decimal = Integer.parseInt(entrada, 2);
+        return Integer.toOctalString(decimal);
+    }
+
+    /**
+     * Función que convierte de binario a hexadecimal.
+     * 
+     * @param entrada Entrada binaria
+     * @return Salida hex
+     */
+    public static String binarioAHex(String entrada) {
+        int decimal = Integer.parseInt(entrada, 2);
+        return Integer.toHexString(decimal);
+    }
+
+    /**
+     * Función que convierte de octal a binario.
+     * @param entrada Entrada octal
+     * @return Salida binaria
+     */
+    public static String octalABinario(String entrada) {
+        int decimal = Integer.parseInt(entrada, 8);
+        return Integer.toBinaryString(decimal);
     }
 
     /**
@@ -20,8 +52,65 @@ public class T08E06Conversiones {
      * @param entrada Entrada octal
      * @return Salida decimal
      */
-    public static int octalADecimal(String entrada) {
-        return Integer.parseInt(entrada, 8);
+    public static String octalADecimal(String entrada) {
+        return Integer.toString(Integer.parseInt(entrada, 8));
+    }
+
+    /**
+     * Función que convierte de octal a hexadecimal.
+     * @param entrada Entrada octal
+     * @return Salida hex
+     */
+    public static String octalAHex(String entrada) {
+        int decimal = Integer.parseInt(entrada, 8);
+        return Integer.toHexString(decimal);
+    }
+
+    /**
+     * Función que convierte de decimal a binario.
+     * @param entrada Entrada Decimal
+     * @return Salida binaria
+     */
+    public static String decimalABinario(String entrada) {
+        return Integer.toBinaryString(Integer.parseInt(entrada));
+    }
+
+    /**
+     * Función que convierte de decimal a octal.
+     * @param entrada Entrada octal
+     * @return Salida binaria
+     */
+    public static String decimalAOctal(String entrada) {
+        return Integer.toOctalString(Integer.parseInt(entrada));
+    }
+
+    /**
+     * Función que convierte de decimal a hexadecimal.
+     * @param entrada Entrada hex
+     * @return Salida binaria
+     */
+    public static String decimalAHex(String entrada) {
+        return Integer.toHexString(Integer.parseInt(entrada));
+    }
+
+    /**
+     * Función que convierte de hexadecimal a binario.
+     * @param entrada Entrada hex
+     * @return Salida binaria
+     */
+    public static String hexABinario(String entrada) {
+        int decimal = Integer.parseInt(entrada, 16);
+        return Integer.toBinaryString(decimal);
+    }
+
+    /**
+     * Función que convierte de hexadecimal a octal.
+     * @param entrada Entrada hex
+     * @return Salida octal
+     */
+    public static String hexAOctal(String entrada) {
+        int decimal = Integer.parseInt(entrada, 16);
+        return Integer.toOctalString(decimal);
     }
 
     /**
@@ -29,8 +118,9 @@ public class T08E06Conversiones {
      * @param entrada Entrada hex
      * @return Salida decimal
      */
-    public static int hexADecimal(String entrada) {
-        return Integer.parseInt(entrada, 16);
+    public static String hexADecimal(String entrada) {
+        int decimal = Integer.parseInt(entrada, 16);
+        return Integer.toString(decimal);
     }
 
 
--- Tema8Funciones/T08E06MainConversiones.java
+++ Tema8Funciones/T08E06MainConversiones.java
@@ -26,15 +26,50 @@ public interface T08E06MainConversiones {
 
             System.out.print("Elija una opción: ");
             opcion = sc.nextLine();
-            String entrada = "";
-            switch (opcion) {
+            switch (opcion.toUpperCase()) {
                 case "A":
                     System.out.print("Introduzna un número binario: ");
+                    System.out.println(T08E06Conversiones.binarioAOctal(sc.nextLine()));
+                    break;
+                case "B":
+                    System.out.print("Introduzna un número binario: ");
                     System.out.println(T08E06Conversiones.binarioADecimal(sc.nextLine()));
+                    break;
+                case "C":
+                    System.out.print("Introduzna un número binario: ");
+                    System.out.println(T08E06Conversiones.binarioAHex(sc.nextLine()));
+                    break;
+                case "D":
+                    System.out.print("Introduzna un número octal: ");
+                    System.out.println(T08E06Conversiones.octalABinario(sc.nextLine()));
                     break;
                 case "E":
                     System.out.print("Introduzna un número octal: ");
                     System.out.println(T08E06Conversiones.octalADecimal(sc.nextLine()));
+                    break;
+                case "F":
+                    System.out.print("Introduzna un número octal: ");
+                    System.out.println(T08E06Conversiones.octalAHex(sc.nextLine()));
+                    break;
+                case "G":
+                    System.out.print("Introduzna un número decimal: ");
+                    System.out.println(T08E06Conversiones.decimalABinario(sc.nextLine()));
+                    break;
+                case "H":
+                    System.out.print("Introduzna un número decimal: ");
+                    System.out.println(T08E06Conversiones.decimalAOctal(sc.nextLine()));
+                    break;
+                case "I":
+                    System.out.print("Introduzna un número decimal: ");
+                    System.out.println(T08E06Conversiones.decimalAHex(sc.nextLine()));
+                    break;
+                case "J":
+                    System.out.print("Introduzna un número hexadecimal: ");
+                    System.out.println(T08E06Conversiones.hexABinario(sc.nextLine()));
+                    break;
+                case "K":
+                    System.out.print("Introduzna un número hexadecimal: ");
+                    System.out.println(T08E06Conversiones.hexAOctal(sc.nextLine()));
                     break;
                 case "L":
                     System.out.print("Introduzna un número hexadecimal: ");
@@ -44,7 +79,7 @@ public interface T08E06MainConversiones {
                     break;
             }
 
-        } while (!opcion.equals("T"));
+        } while (!opcion.toUpperCase().equals("T"));
 
         // Cierra el scanner
         sc.close();
