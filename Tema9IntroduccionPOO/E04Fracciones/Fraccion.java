a69bacf0e034e197f8eaad7d84e2bf7202f2ce76
Javier Caselli <7252806+javiercaselli@users.noreply.github.com>
2026-02-17 17:26:07 +0100

    Ejercicios POO 4 completo.

--- Tema9IntroduccionPOO/E04Fracciones/Fraccion.java
+++ Tema9IntroduccionPOO/E04Fracciones/Fraccion.java
@@ -13,14 +13,82 @@ public class Fraccion {
     }
 
     // metodos
+
+    /**
+     * Devuelve la fraccion inversa de la que se llama.
+     *
+     * @return La fraccion inversa
+     */
     public Fraccion invertir() {
         return new Fraccion(denominador, numerador);
     }
 
+    /**
+     * Devuelve la multiplicacion de dos fracciones.
+     * @param otra La fraccion con la que se va a multiplicar.
+     * @return La multiplicacion de las dos fracciones.
+     */
     public Fraccion multiplicar(Fraccion otra) {
-        return new Fraccion(numerador * otra.numerador, denominador * otra.denominador);
+        return (new Fraccion(numerador * otra.numerador, denominador * otra.denominador)).simplificar();
     }
 
+    /**
+     * Devuelve la division de dos fracciones.
+     * @param otra La fraccion con la que se va a dividir.
+     * @return La division de las dos fracciones.
+     */
+    public Fraccion dividir(Fraccion otra) {
+        return (new Fraccion(numerador * otra.denominador, denominador * otra.numerador)).simplificar();
+    }
+
+    /**
+     * Devuelve la suma de dos fracciones.
+     * @param otra La fraccion con la que se va a sumar.
+     * @return La suma de las dos fracciones.
+     */
+    public Fraccion sumar(Fraccion otra) {
+        Fraccion temp = new Fraccion((numerador * otra.denominador) + (otra.numerador * denominador), denominador * otra.denominador);
+        return temp.simplificar();
+    }
+
+    /**
+     * Devuelve la fraccion simplificada.
+     * @return La fraccion simplificada.
+     */
+    public Fraccion simplificar() {
+        int mcd = MCD(numerador, denominador);
+        return new Fraccion(numerador / mcd, denominador / mcd);
+    }
+
+    /**
+     * Devuelve el MCD de dos numeros enteros.
+     * @param a
+     * @param b
+     * @return El MCD de a y b.
+     */
+    private int MCD(int a, int b) {
+        while (b != 0) {
+            int aux = b;
+            b = a % b;
+            a = aux;
+        }
+        return a;
+    }
+
+    /**
+     * Devuelve el MCD de dos numeros enteros recursivamente.
+     * @param a
+     * @param b
+     * @return El MCD de a y b.
+     */
+    private int MCDRecursivo(int a, int b) {
+        return b == 0 ? a : MCDRecursivo(b, a % b);
+    }
+
+    /**
+     * Devuelve una cadena con el formato "numerador/denominador".
+     * @return La cadena con el formato "numerador/denominador".
+     */
     public String toString() {
         return numerador + "/" + denominador;
     }
--- Tema9IntroduccionPOO/E04Fracciones/MainFraccion.java
+++ Tema9IntroduccionPOO/E04Fracciones/MainFraccion.java
@@ -3,12 +3,26 @@ package Tema9IntroduccionPOO.E04Fracciones;
 public class MainFraccion {
 
     public static void main(String[] args) {
-        Fraccion f1 = new Fraccion(1, 2);
-        Fraccion f2 = new Fraccion(4, 3);
+        Fraccion f1 = new Fraccion(-7, 8);
+        Fraccion f2 = new Fraccion(5, 1);
 
-        System.out.println(f1);
-        System.out.println(f1.invertir());
-        System.out.println(f1.multiplicar(f2));
+
+        System.out.println(f1 + " * " + f2 + " = " + f1.multiplicar(f2));
+        System.out.println(f1 + " ^-1 = " + f1.invertir());
+
+        Fraccion f3 = new Fraccion(3, 5);
+        System.out.println(f1 + " * " + f3 + " = " + f1.multiplicar(f3));
+
+        System.out.println(f1 + " + " + f3 + " = " + f1.sumar(f3));
+
+        // Ejemplos con simplificación
+        Fraccion f4 = new Fraccion(18, 8);
+        Fraccion f5 = new Fraccion(26, 14);
+
+        System.out.println();
+        System.out.println(f4 + " + " + f5 + " = " + f4.sumar(f5));
+        System.out.println(f4 + " * " + f5 + " = " + f4.multiplicar(f5));
+
     }
 
 }
